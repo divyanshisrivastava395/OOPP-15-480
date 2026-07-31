c26e5ca62e600b114a35b6d8423df09946ad09c2
Divyanshi Srivastava <241068726+divyanshisrivastava395@users.noreply.github.com>
2026-07-31 09:31:15 +0530

    initial commit

--- prg4.cpp
+++ prg4.cpp
@@ -3,14 +3,27 @@ using namespace std;
 
 class BankAccount{
     private:
-    int amt;
+    int amt=0;
 string pin ,name;
 public:
-void withdraw(int a){
+   void show(){
+
+   } 
+void withdraw(int rs){
+amt-=rs;
 
 }
-void deposit(int a){
-    
+void deposit(int rs){
+amt+=rs;
 }
 
 };
+int main(){
+    BankAccount a,b,c;;
+    a.deposit(25000);
+    b.withdraw(10000);
+    a.amt=0;
+    a.name="Ravi";
+cout<<c.amt<<endl;
+    
+}
